c87a327dbacf7c803fefb07693e6ad70e4fc7b9b
matousss <matousss33@gmail.com>
2022-06-12 20:31:18 +0200

    Datagens

--- random_datagen.py
+++ random_datagen.py
@@ -8,7 +8,7 @@ from randomized.player import RandomPlayer
 
 if __name__ == '__main__':
     theme('black')
-    t = Trainer(RandomPlayer(0), MiniMaxPlayer(1), max_games=20)
+    t = Trainer(RandomPlayer(0), MiniMaxPlayer(1), max_games=5000)
     t.start()
 
     data = data_from_logs(t.get_logs())
