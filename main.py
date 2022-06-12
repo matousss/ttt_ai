95654d30d6abd4ec45caa890e6f054e17f3b2389
matousss <matousss33@gmail.com>
2022-06-12 17:46:27 +0200

    Added user train config

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from time import sleep
-
 from PySimpleGUI import theme
 
-from ai.data import data_from_logs, save_data
-from ai.trainer import TrainerGUI
+from minimax.player import MiniMaxPlayer
 from tictactoe.players import HumanPlayer
 from tictactoe.game import TicTacToeGUI
 
 if __name__ == '__main__':
     theme('black')
-    t = TrainerGUI(HumanPlayer(0), HumanPlayer(1), choose_next_player=lambda game: 1)
+    t = TicTacToeGUI(HumanPlayer(0), MiniMaxPlayer(1), max_games=20)
     t.start()
-    while t.is_running():
-        sleep(.1)
 
-    data = data_from_logs(t.get_logs())
-
-    save_data(*data, 'data.txt')
+    print(t.get_scores())
--- user_train.py
+++ user_train.py
@@ -1,19 +1,14 @@
-from time import sleep
-
 from PySimpleGUI import theme
 
-from ai.data import data_from_logs, save_data, load_data
+from ai.data import data_from_logs, save_data
 from ai.trainer import TrainerGUI
+from minimax.player import MiniMaxPlayer
 from tictactoe.players import HumanPlayer
 
 if __name__ == '__main__':
     theme('black')
-    t = TrainerGUI(HumanPlayer(0), HumanPlayer(1))
+    t = TrainerGUI(HumanPlayer(0), MiniMaxPlayer(1), max_games=20)
     t.start()
-    while t.is_running():
-        sleep(.1)
 
     data = data_from_logs(t.get_logs())
-
-    save_data(*data, 'data.txt')
-    print(load_data('data.txt'))
+    save_data(*data, 'data_player-minimax.txt')
