ea949f2eba792fd4d781d6bc5a978d8a9b59d3fe
matousss <matousss33@gmail.com>
2022-06-14 16:46:32 +0200

    Some tests

--- play_ai.py
+++ play_ai.py
@@ -3,6 +3,7 @@ from PySimpleGUI import theme
 from keras.saving.save import load_model
 
 from ai.player import AIPlayer
+from minimax.minimax import best_move
 from tictactoe.game import TicTacToeGUI
 from tictactoe.players import HumanPlayer
 
@@ -10,14 +11,17 @@ if __name__ == '__main__':
     theme('black')
     model = load_model('model.h5')
     # TicTacToeGUI(HumanPlayer, AIPlayerBuilder(model)).start()
-    d = numpy.array([
+    n =[
         [0, -1, 0],
         [0, 0, 0],
         [0, 0, 0],
-    ]).reshape(-1, 9)
+    ]
+    d = numpy.array(n).reshape(-1, 9)
 
     print(d)
-
+    print(best_move(n, 1))
     p = model.predict(d)
 
     print(p)
+    print(p[0][0])
+    print(max(p[0]))
