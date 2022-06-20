2314ea469831ee7e75ea4b5794069ee859e7f549
matousss <matousss33@gmail.com>
2022-06-20 21:31:33 +0200

    Smol change

--- train_model.py
+++ train_model.py
@@ -4,7 +4,7 @@ from ai.data import get_raw_data
 from ai.model import create_model, train
 
 if __name__ == '__main__':
-    raw = get_raw_data('./data/data_random-minimax.txt')
+    raw = get_raw_data('./data.txt')
     data_x, data_y = numpy.array(raw[0]), numpy.array(raw[1])
 
     # model = create_model(hidden_layers=[12, 14, 14, 12])
